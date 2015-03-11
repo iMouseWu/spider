8ad20427bdf87bbb284218956279194bde2fa0cf
iMouseWu <2159201@qq.com>
2015-03-11 13:23:04 +0800

    淘宝页面分析

---
+++ spider-main/src/main/java/com/spider/handle/TbHtmlHandle.java
@@ -0,0 +1,13 @@
+package com.spider.handle;
+
+import com.spider.domain.Item;
+
+
+public class TbHtmlHandle   {
+
+	public Item analyze(String html) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
